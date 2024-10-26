71b302b7e449727977d7091365c2642ab79bee7e
lohieth-rvrl <lohiethrvrl212@gmail.com>
2024-10-26 11:21:44 +0530

    loki added

--- course/views.py
+++ course/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import *
 from django.contrib import messages
 # Create your views here.
 
@@ -11,11 +12,43 @@ def home(request):
 
 
 # login & register view
+
+# def login_view(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(request, data=request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data.get('username')
+#             password = form.cleaned_data.get('password')
+#             user = authenticate(username=username, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 messages.success(request, f"Welcome, {username}!")
+#                 return redirect('home')  # Redirect to a home or dashboard page
+#             else:
+#                 messages.error(request, "Invalid username or password.")
+#         else:
+#             messages.error(request, "Invalid username or password.")
+#     else:
+#         form = AuthenticationForm()
+    
+#     return render(request, 'course/login/login.html', {'form': form})
 def login(request):
-    return render(request,"course/login/login.html")
+    return render(request,"course/registration/login.html")
 
 def register(request):
-    return render(request,'course/login/register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            # Hash the password before saving (Django's auth should ideally be used for this)
+            user.password = form.cleaned_data['password']
+            user.save()
+            messages.success(request, "Registration successful!")
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    
+    return render(request, 'course/registration/register.html', {'form': form})
 
 
 # course View
